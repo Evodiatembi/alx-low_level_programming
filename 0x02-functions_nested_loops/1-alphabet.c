28863988e2a34141d4b32470f491e8221325d00d
Evodiatembi <evodytembi@gmail.com>
2023-09-12 15:03:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*main -main function
+*Return: always 0
+*/
+void print_alphabet_x10(void);
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+return (0);
+}
+
