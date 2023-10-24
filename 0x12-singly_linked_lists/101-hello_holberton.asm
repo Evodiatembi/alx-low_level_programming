c47041adf054fabd41c65100090584bd7ab383ba
Evodiatembi <evodytembi@gmail.com>
2023-10-23 21:30:15 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,145 @@
+global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0global    main
+
+          extern    printf
+
+main:
+
+          mov   edi, format
+
+          xor   eax, eax
+
+          call  printf
+
+          mov         eax, 0
+
+          ret
+
+format: db `Hello, Holberton\n`,0
